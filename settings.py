26c26fba9f7a0013834553732513eb3460b89e07
matyuji <yuji.matsumoto@gu3.co.jp>
2012-02-09 18:53:17 +0900

    スペース追加（ダミー）

--- settings.py
+++ settings.py
@@ -20,10 +20,11 @@ DATABASES = {
         'NAME': 'mysite',        	            # Or path to database file if using sqlite3.
         'USER': 'root',                  	    # Not used with sqlite3.
         'PASSWORD': 'gumi',              	    # Not used with sqlite3.
-        'HOST': '/tmp/mysqld.sock',            	# Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',            	                # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      	    # Set to empty string for default. Not used with sqlite3.
     }
 }
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
