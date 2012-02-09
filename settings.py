8b29666107b520c9882946acdee0b7141a1a46a8
makoto takeuchi <makoto.takeuchi@gu3.co.jp>
2012-02-09 20:44:40 +0900

    いらないファイル消した settings.pyをみんなが使える用に修正

--- settings.py
+++ settings.py
@@ -18,13 +18,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 	# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'mysite',        	            # Or path to database file if using sqlite3.
-        'USER': 'root',                  	    # Not used with sqlite3.
-        'PASSWORD': 'gumi',              	    # Not used with sqlite3.
+        'USER': '',                       	    # Not used with sqlite3.
+        'PASSWORD': '',                    	    # Not used with sqlite3.
         'HOST': '',            	                # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      	    # Set to empty string for default. Not used with sqlite3.
     }
 }
-
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
